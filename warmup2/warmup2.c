52f7a49a75d5fb89718d6cc0b1c0fd66f448a234
Jason Liu <chliu.official@gmail.com>
2022-02-06 21:18:16 +0800

    edit warmup2 arg parsing

--- warmup2/warmup2.c
+++ warmup2/warmup2.c
@@ -24,12 +24,16 @@ int B = 0;
 int P = 0;
 int num = 0;
 char* tsfileName = NULL;
+FILE* fp = NULL;
 
 void ParseArgs(int, char**);
-double ValidatePositiveRealNum(char*, char*);
-int ValidatePositiveInteger(char*, char*);
+void ValidateUniqueArg(char*, double);
+void ValidateParam(char*, double, char*);
+double StrToPositiveRealNum(char*);
+int StrToPositiveInteger(char*);
 void InitParams();
 void PrintParams();
+void OpenTSFile();
 
 int main(int argc, char* argv[])
 {   
@@ -37,58 +41,45 @@ int main(int argc, char* argv[])
     InitParams();
     PrintParams();
 
-    
     return 0;
 }
 
 void ParseArgs(int argc, char** argv) {
     for (int i = 1; i < argc; i += 2) {
         if (i + 1 >= argc) {
-            fprintf(stderr, "Error: argument %s missing value!!\n", argv[i]);
+            fprintf(stderr, "Error: argument \"%s\" missing value!!\n", argv[i]);
             exit(-1);
         }
 
         if (strcmp(argv[i], "-lambda") == 0) {
-            if (lambda != 0) {
-                fprintf(stderr, "Error: duplicate argument \"%s\"!!\n", argv[i]);
-                exit(-1);
-            }
-            lambda = ValidatePositiveRealNum(argv[i], argv[i+1]);
+            ValidateUniqueArg(argv[i], lambda);
+            lambda = StrToPositiveRealNum(argv[i+1]);
+            ValidateParam(argv[i], lambda, "real number");
         }
         else if (strcmp(argv[i], "-mu") == 0) {
-            if (mu != 0) {
-                fprintf(stderr, "Error: duplicate argument \"%s\"!!\n", argv[i]);
-                exit(-1);
-            }
-            mu = ValidatePositiveRealNum(argv[i], argv[i+1]);
+            ValidateUniqueArg(argv[i], mu);
+            mu = StrToPositiveRealNum(argv[i+1]);
+            ValidateParam(argv[i], mu, "real number");
         }
         else if (strcmp(argv[i], "-r") == 0) {
-            if (r != 0) {
-                fprintf(stderr, "Error: duplicate argument \"%s\"!!\n", argv[i]);
-                exit(-1);
-            }
-            r = ValidatePositiveRealNum(argv[i], argv[i+1]);
+            ValidateUniqueArg(argv[i], r);
+            r = StrToPositiveRealNum(argv[i+1]);
+            ValidateParam(argv[i], r, "real number");
         }
         else if (strcmp(argv[i], "-B") == 0) {
-            if (B != 0) {
-                fprintf(stderr, "Error: duplicate argument \"%s\"!!\n", argv[i]);
-                exit(-1);
-            }
-            B = ValidatePositiveInteger(argv[i], argv[i+1]);
+            ValidateUniqueArg(argv[i], (double) B);
+            B = StrToPositiveInteger(argv[i+1]);
+            ValidateParam(argv[i], B, "integer");
         }
         else if (strcmp(argv[i], "-P") == 0) {
-            if (P != 0) {
-                fprintf(stderr, "Error: duplicate argument \"%s\"!!\n", argv[i]);
-                exit(-1);
-            }
-            P = ValidatePositiveInteger(argv[i], argv[i+1]);
+            ValidateUniqueArg(argv[i], (double) P);
+            P = StrToPositiveInteger(argv[i+1]);
+            ValidateParam(argv[i], P, "integer");
         }
         else if (strcmp(argv[i], "-n") == 0) {
-            if (num != 0) {
-                fprintf(stderr, "Error: duplicate argument \"%s\"!!\n", argv[i]);
-                exit(-1);
-            }
-            num = ValidatePositiveInteger(argv[i], argv[i+1]);
+            ValidateUniqueArg(argv[i], (double) num);
+            num = StrToPositiveInteger(argv[i+1]);
+            ValidateParam(argv[i], num, "integer");
         }
         else if (strcmp(argv[i], "-t") == 0) {
             if (tsfileName != NULL) {
@@ -97,36 +88,44 @@ void ParseArgs(int argc, char** argv) {
             }
             tsfileName = (char*) malloc(strlen(argv[i+1]) + 1);
             strncpy(tsfileName, argv[i+1], strlen(argv[i+1]) + 1);
+            OpenTSFile();
         }
         else {
             fprintf(stderr, "Error: unknown argument \"%s\"!!\n", argv[i]);
             fprintf(stderr, "Usage: warmup2 [-lambda lambda] [-mu mu] [-r r] [-B B] [-P P] [-n num] [-t tsfile]\n");
             exit(-1);
         }
-
     }
 }
 
-double ValidatePositiveRealNum(char* arg, char* val) {
+void ValidateUniqueArg(char* arg, double var) {
+    if (var != 0) {
+        fprintf(stderr, "Error: duplicate argument \"%s\"!!\n", arg);
+        exit(-1);
+    }
+}
+
+void ValidateParam(char* arg, double val, char* type) {
+    if (val == -1) {
+        fprintf(stderr, "Error: value of argument \"%s\" must be a positive %s!!\n", arg, type);
+        exit(-1);
+    }
+}
+
+double StrToPositiveRealNum(char* val) {
     char* endPtr;
     double number = strtod(val, &endPtr);
     if (number == 0.0 || *endPtr != '\0') {
-        fprintf(stderr, "Error: argument \"%s\" must be a positive real number!!\n", arg);
-        exit(-1);
+        return -1;
     }
     return number;
 }
 
-int ValidatePositiveInteger(char* arg, char* val) {
+int StrToPositiveInteger(char* val) {
     char* endPtr;
     long long number = strtoll(val, &endPtr, 10);
-    if (number == 0LL || *endPtr != '\0') {
-        fprintf(stderr, "Error: argument \"%s\" must be a positive integer!!\n", arg);
-        exit(-1);
-    }
-    if (number > INT32_MAX) {
-        fprintf(stderr, "Error: argument \"%s\" exceeds maximum value (%d)!!\n", arg, INT32_MAX);
-        exit(-1);
+    if (number == 0LL || number > INT32_MAX || *endPtr != '\0') {
+        return -1;
     }
     return (int) number;
 }
@@ -168,5 +167,17 @@ void PrintParams() {
     }
     if (tsfileName != NULL) {
         printf("    tsfile = %s\n\n", tsfileName);
+    }
+}
+
+void OpenTSFile() {
+    if (opendir(tsfileName) != NULL) {
+        fprintf(stderr, "Error: given file path is a directory (%s)!!\n", tsfileName);
+        exit(-1);
+    }
+    fp = fopen(tsfileName, "r");
+    if (fp == NULL) {
+        fprintf(stderr, "Error: cannot open file (%s)!!\n", tsfileName);
+        exit(-1);
     }
 }
